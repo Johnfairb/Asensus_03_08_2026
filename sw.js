ea16080b8a56cc47c5a34dc8e3a29afd8f96820b
John <johnfairbair67@gmail.com>
2026-08-07 10:17:40 +0100

    "Hopefully last fix"

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Ascensus Offline GPS Cache (The Tunnel Protocol)
-const CACHE_NAME = 'ascensus-gps-v44';
+const CACHE_NAME = 'ascensus-gps-v45';
 
 // The critical assets required to render the UI offline
 const ASSETS_TO_CACHE = [
